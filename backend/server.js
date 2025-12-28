847fdc669920cc1f289bd6d64b9807b108773d6e
jimmy <jimmiparekh936@gmail.com>
2025-12-28 19:56:21 +0530

    validation check  added for DB_URL

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+dotenv.config(); // MUST be first
+
+import express from "express";
+import path from "path";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
+
+// API ROUTES
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ msg: "API is running" });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Books API working" });
+});
+
+// PRODUCTION MODE
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
+}
+
+// START SERVER
+const startServer = async () => {
+  try {
+    app.listen(PORT, async () => {
+      console.log(`Server running on port: ${PORT}`);
+      await connectDB(); // Connect to MongoDB
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
