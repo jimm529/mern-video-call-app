108c4e2db873ebfc4e9c484b8d7d78400061b0c1
jimmy <jimmiparekh936@gmail.com>
2025-12-29 11:19:19 +0530

    Initial commit

---
+++ backend/lib/ingest.js
@@ -0,0 +1,58 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../modles/User.js";
+
+export const inngest = new inngest({ id: "INTERVIEWX" });
+
+const syncUser = inngest.createfuntion
+    (
+        { id: "sync-user" },
+        { event: "clerk/user.created" },
+        async ({ event }) => {
+
+            await connectDB()
+
+
+
+            const newUser = {
+
+                clearkId: id,
+                email: email_addresses[0]?.email_addresses,
+                name: `${first_name || ""} ${last_name || ""}`,
+                profileImage: image_url
+            }
+            const { id, email_addresses, first_name, last_name, image_url } = event.data
+
+        }
+
+
+
+
+    )
+
+const deleteUSerFromDB = inngest.createfuntion
+    (
+        { id: "delete-user-from-db" },
+        { event: "clerk/user.delete" },
+        async ({ event }) => {
+
+            await connectDB()
+
+
+
+            const newUser = {
+
+                clearkId: id,
+                email: email_addresses[0]?.email_addresses,
+                name: `${first_name || ""} ${last_name || ""}`,
+                profileImage: image_url
+            }
+            const { id, email_addresses, first_name, last_name, image_url } = event.data
+
+        }
+
+    )
+
+
+
+export const functions = { syncUser ,deleteUSerFromDB};
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ dotenv.config(); // MUST be first
 
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { ENV } from "./lib/env.js";
+import {serve} from "inngest/express"
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -15,10 +17,13 @@ app.get("/api/health", (req, res) => {
   res.status(200).json({ msg: "API is running" });
 });
 
+
+app.use("/api/inngest",serve);
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Books API working" });
 });
-
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 // PRODUCTION MODE
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
